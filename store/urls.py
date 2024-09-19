ca418735a6ebf506f795f53f327830449f9f8dea
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-19 18:35:19 +0300

    Create index view

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from store.views import (
+    index,
+)
+
 
 urlpatterns = [
-
+    path("", index, name="index")
 ]
 
 app_name = "store"
--- store/views.py
+++ store/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from store.models import (
+    Product,
+    Order,
+    Brand,
+)
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_customers = get_user_model().objects.count()
+    num_products = Product.objects.count()
+    num_brands = Brand.objects.count()
+    num_orders = Order.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_customers": num_customers,
+        "num_products": num_products,
+        "num_brands": num_brands,
+        "num_orders": num_orders,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "store/index.html", context=context)
