449779bf58331c8e8434ff1d1c2c2836f7477100
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-19 13:54:16 +0300

    Add product model

--- store/models.py
+++ store/models.py
@@ -11,3 +11,31 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class Product(models.Model):
+    UNITS = (
+        ("KG", "kilograms"),
+        ("G", "grams"),
+        ("L", "liters"),
+        ("ML", "Milliliters"),
+        ("PCS", "pieces"),
+        ("BOX", "box")
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    unit_value = models.FloatField()
+    unit_name = models.CharField(max_length=3, choices=UNITS)
+    stock_quantity = models.FloatField(default=0)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.unit_value} {self.unit_name}"
