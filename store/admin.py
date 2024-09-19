f291a5f927822d9c009ed44a32bb27999a4c0fe6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-19 17:26:45 +0300

    Add Product, Category & Brand models to admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Product, Category, Brand
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "brand",
+        "unit_value",
+        "unit_name",
+        "stock_quantity",
+        "price",
+        "category",
+    ]
+    list_filter = ["brand", "category", ]
+    search_fields = ["name", ]
+    list_per_page = 10
+
+
+admin.site.register(Category)
+admin.site.register(Brand)
