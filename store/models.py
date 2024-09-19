21185806ef5f55759c43570bbf0da9aaf92ec626
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-19 13:46:43 +0300

    Add category model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField(null=True)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self) -> str:
+        return self.name
