632d8fe2bec14cc9f10e44515145bcdb949a7eeb
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-21 18:04:45 +0300

    Add product search form to forms.py

--- store/forms.py
+++ store/forms.py
@@ -59,3 +59,16 @@ class OrderStatusForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ("status",)
+
+
+class ProductSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name..."
+            }
+        )
+    )
