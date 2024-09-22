613f3014fde42f1e62c70a7ed4e8ce626c4630ca
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-23 01:37:24 +0300

    Add login requirement for all pages, except index view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models import Case, When, IntegerField, QuerySet
 from django.http import HttpRequest, HttpResponse
@@ -38,7 +40,7 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "store/index.html", context=context)
 
 
-class ProductListView(generic.ListView):
+class ProductListView(LoginRequiredMixin, generic.ListView):
     model = Product
     paginate_by = 5
 
@@ -88,7 +90,7 @@ class ProductListView(generic.ListView):
         return queryset
 
 
-class ProductDetailView(generic.DetailView):
+class ProductDetailView(LoginRequiredMixin, generic.DetailView):
     model = Product
     queryset = Product.objects.select_related("brand", "category").prefetch_related("reviews__customer")
 
@@ -117,7 +119,7 @@ class ProductDetailView(generic.DetailView):
         return self.render_to_response(context)
 
 
-class ProductCreateView(generic.CreateView):
+class ProductCreateView(LoginRequiredMixin, generic.CreateView):
     model = Product
     fields = "__all__"
 
@@ -125,7 +127,7 @@ class ProductCreateView(generic.CreateView):
         return reverse("store:product-detail", args=[self.object.pk])
 
 
-class ProductUpdateView(generic.UpdateView):
+class ProductUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Product
     fields = "__all__"
 
@@ -133,12 +135,12 @@ class ProductUpdateView(generic.UpdateView):
         return reverse("store:product-detail", args=[self.object.pk])
 
 
-class ProductDeleteView(generic.DeleteView):
+class ProductDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Product
     success_url = reverse_lazy("store:product-list")
 
 
-class OrderListView(generic.ListView):
+class OrderListView(LoginRequiredMixin, generic.ListView):
     model = Order
     paginate_by = 5
     queryset = Order.objects.prefetch_related("order_items__product", "customer")
@@ -149,63 +151,63 @@ class OrderListView(generic.ListView):
         return context
 
 
-class OrderDetailView(generic.DetailView):
+class OrderDetailView(LoginRequiredMixin, generic.DetailView):
     model = Order
     queryset = Order.objects.prefetch_related("order_items__product", "customer")
 
 
-class CategoryListView(generic.ListView):
+class CategoryListView(LoginRequiredMixin, generic.ListView):
     model = Category
     paginate_by = 5
 
 
-class CategoryCreateView(generic.CreateView):
+class CategoryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Category
     fields = "__all__"
     success_url = reverse_lazy("store:category-list")
 
 
-class CategoryUpdateView(generic.UpdateView):
+class CategoryUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Category
     fields = "__all__"
     success_url = reverse_lazy("store:category-list")
 
 
-class CategoryDeleteView(generic.DeleteView):
+class CategoryDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Category
     success_url = reverse_lazy("store:category-list")
 
 
-class BrandListView(generic.ListView):
+class BrandListView(LoginRequiredMixin, generic.ListView):
     model = Brand
     paginate_by = 5
     queryset = Brand.objects.prefetch_related("products")
 
 
-class BrandCreateView(generic.CreateView):
+class BrandCreateView(LoginRequiredMixin, generic.CreateView):
     model = Brand
     fields = "__all__"
     success_url = reverse_lazy("store:brand-list")
 
 
-class BrandUpdateView(generic.UpdateView):
+class BrandUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Brand
     fields = "__all__"
     success_url = reverse_lazy("store:brand-list")
 
 
-class BrandDeleteView(generic.DeleteView):
+class BrandDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Brand
     success_url = reverse_lazy("store:brand-list")
 
 
-class CustomerListView(generic.ListView):
+class CustomerListView(LoginRequiredMixin, generic.ListView):
     model = get_user_model()
     paginate_by = 5
     queryset = get_user_model().objects.prefetch_related("orders")
 
 
-class CustomerDetailView(generic.DetailView):
+class CustomerDetailView(LoginRequiredMixin, generic.DetailView):
     model = get_user_model()
 
 
@@ -215,7 +217,7 @@ class CustomerDetailView(generic.DetailView):
         return context
 
 
-class CustomerCreateView(generic.CreateView):
+class CustomerCreateView(LoginRequiredMixin, generic.CreateView):
     model = get_user_model()
     form_class = CustomerCreationForm
 
@@ -223,7 +225,7 @@ class CustomerCreateView(generic.CreateView):
         return reverse("store:customer-detail", args=[self.object.pk])
 
 
-class CustomerUpdateView(generic.UpdateView):
+class CustomerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = get_user_model()
     fields = ["first_name", "last_name", "phone_number", "address"]
 
@@ -231,11 +233,12 @@ class CustomerUpdateView(generic.UpdateView):
         return reverse("store:customer-detail", args=[self.object.pk])
 
 
-class CustomerDeleteView(generic.DeleteView):
+class CustomerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = get_user_model()
     success_url = reverse_lazy("store:customer-list")
 
 
+@login_required
 def cart_detail(request: HttpRequest) -> HttpResponse:
     shopping_cart, created = ShoppingCart.objects.get_or_create(customer=request.user)
     cart_items = shopping_cart.cart_items.select_related("product")
@@ -248,6 +251,7 @@ def cart_detail(request: HttpRequest) -> HttpResponse:
     return render(request, "store/cart_detail.html", context=context)
 
 
+@login_required
 def add_to_cart(request: HttpRequest, product_id: int) -> HttpResponse:
     product = get_object_or_404(Product, id=product_id)
     shopping_cart, created = ShoppingCart.objects.get_or_create(customer=request.user)
@@ -268,6 +272,7 @@ def add_to_cart(request: HttpRequest, product_id: int) -> HttpResponse:
     return redirect(request.META.get("HTTP_REFERER", reverse_lazy("store:index")))
 
 
+@login_required
 def delete_from_cart(request: HttpRequest, product_id: int) -> HttpResponse:
     product = get_object_or_404(Product, id=product_id)
     shopping_cart = ShoppingCart.objects.get(customer=request.user)
@@ -289,6 +294,7 @@ def delete_from_cart(request: HttpRequest, product_id: int) -> HttpResponse:
     return redirect(request.META.get("HTTP_REFERER", reverse_lazy("store:index")))
 
 
+@login_required
 def empty_cart(request: HttpRequest) -> HttpResponse:
     shopping_cart = ShoppingCart.objects.get(customer=request.user)
     shopping_cart.cart_items.all().delete()
@@ -296,6 +302,7 @@ def empty_cart(request: HttpRequest) -> HttpResponse:
     return redirect("store:cart-detail")
 
 
+@login_required
 def update_order_status(request: HttpRequest, pk: int) -> HttpResponse:
     order = get_object_or_404(Order, pk=pk)
     if request.method == "POST":
@@ -307,6 +314,7 @@ def update_order_status(request: HttpRequest, pk: int) -> HttpResponse:
     return redirect("store:order-list")
 
 
+@login_required
 def create_order_from_cart(request: HttpRequest) -> HttpResponse:
     shopping_cart = get_object_or_404(ShoppingCart, customer=request.user)
     cart_items = CartItem.objects.prefetch_related("product").filter(shopping_cart=shopping_cart)
