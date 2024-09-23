35d01ec5ec2c4f085493afcfcd41febab9046d7f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-23 12:24:36 +0300

    Add test for Category model

--- store/tests/tests_models.py
+++ store/tests/tests_models.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from store.models import Category
+
+
+class TestCategoryModel(TestCase):
+    def test_str(self) -> None:
+        category = Category.objects.create(
+            name="Test name",
+        )
+        self.assertEqual(
+            str(category),
+            f"{category.name}"
+        )
