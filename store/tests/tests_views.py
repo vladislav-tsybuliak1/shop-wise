87e2146a60b32b880489f80dc2f8d4e7275c4b97
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-23 16:38:16 +0300

    Add tests for private product views

--- store/tests/tests_views.py
+++ store/tests/tests_views.py
@@ -1,11 +1,16 @@
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
+from django.db.models import Case, When, IntegerField
 from django.test import TestCase
 from django.urls import reverse
 
-from store.models import Product, Brand, Order
+from store.models import Product, Brand, Order, Category, CartItem
 from store.tests.mixins import FixtureMixin, LoginUserMixin
 
+
 ID=1
+PAGINATION = 5
 
 # Index
 INDEX_URL = reverse("store:index")
@@ -186,3 +191,150 @@ class PrivateIndexTest(LoginUserMixin, FixtureMixin, TestCase):
         for num_visits in range(1, visits + 1):
             response = self.client.get(INDEX_URL)
             self.assertEqual(response.context["num_visits"], num_visits)
+
+
+class PrivateProductTest(LoginUserMixin, FixtureMixin, TestCase):
+    def test_list_pagination(self) -> None:
+        response = self.client.get(PRODUCT_LIST_URL)
+        self.assertEqual(
+            len(response.context["product_list"]),
+            PAGINATION
+        )
+
+    def test_list_retrieve_data(self) -> None:
+        response = self.client.get(PRODUCT_LIST_URL)
+        products = Product.objects.annotate(
+            available=Case(
+                When(stock_quantity__gt=0, then=1),
+                default=0,
+                output_field=IntegerField(),
+            )
+        ).order_by("-available", "name")[:PAGINATION]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["product_list"]),
+            list(products)
+        )
+
+    def test_search(self) -> None:
+        search_str = "milka"
+        response = self.client.get(
+            PRODUCT_LIST_URL
+            + f"?name={search_str}"
+        )
+        self.assertEqual(
+            len(response.context["product_list"]),
+            len(Product.objects.filter(name__icontains=search_str))
+        )
+
+    def test_filter_brand_only(self) -> None:
+        brand = Brand.objects.get(pk=ID)
+        response = self.client.get(
+            PRODUCT_LIST_URL
+            + f"?brand={brand.id}"
+        )
+        self.assertEqual(
+            len(response.context["product_list"]),
+            len(Product.objects.filter(brand=brand))
+        )
+
+    def test_filter_category_only(self) -> None:
+        category = Category.objects.get(pk=5)
+        response = self.client.get(
+            PRODUCT_LIST_URL
+            + f"?category={category.id}"
+        )
+        self.assertEqual(
+            len(response.context["product_list"]),
+            len(Product.objects.filter(category=category))
+        )
+
+    def test_filter_and_category(self) -> None:
+        brand = Brand.objects.get(pk=1)
+        category = Category.objects.get(pk=2)
+        response = self.client.get(
+            PRODUCT_LIST_URL
+            + f"?category={category.id}"
+            + f"&?brand={brand.id}"
+        )
+        self.assertEqual(
+            list(response.context["product_list"]),
+            list(Product.objects.filter(category=category, brand=brand))
+        )
+
+    def test_detail(self) -> None:
+        product = Product.objects.get(id=ID)
+        response = self.client.get(PRODUCT_DETAIL_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context["product"].name,
+            product.name
+        )
+        self.assertEqual(
+            response.context["product"].category,
+            product.category
+        )
+        self.assertEqual(
+            response.context["product"].brand,
+            product.brand
+        )
+        self.assertEqual(
+            response.context["product"].stock_quantity,
+            product.stock_quantity
+        )
+
+    def test_create(self) -> None:
+        form_data = {
+            "name": "Test name",
+            "unit_value": 1,
+            "unit_name": "KG",
+            "stock_quantity": 10,
+            "price": Decimal('9.99'),
+            "brand": 1,
+            "category": 1,
+        }
+        response = self.client.post(PRODUCT_CREATE_URL, data=form_data)
+        new_product = Product.objects.filter(name=form_data["name"]).first()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(new_product.name, form_data["name"])
+        self.assertEqual(new_product.stock_quantity, form_data["stock_quantity"])
+        self.assertEqual(new_product.price, form_data["price"])
+        self.assertEqual(new_product.brand.id, form_data["brand"])
+        self.assertEqual(new_product.category.id, form_data["category"])
+
+    def test_update(self) -> None:
+        form_data = {
+            "stock_quantity": 11,
+            "price": Decimal('10.99'),
+        }
+        response = self.client.post(PRODUCT_UPDATE_URL, data=form_data)
+        updated_product = Product.objects.get(pk=ID)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(updated_product.price, form_data["price"])
+        self.assertEqual(updated_product.stock_quantity, form_data["stock_quantity"])
+
+    def test_delete(self) -> None:
+        response = self.client.post(PRODUCT_DELETE_URL)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(
+            Product.objects.filter(id=ID).exists()
+        )
+
+    def test_add_to_cart(self) -> None:
+        product = Product.objects.get(pk=ID)
+        response = self.client.get(CART_ADD_PRODUCT_URL)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(product.name, list(self.user.shopping_cart.cart_items.values_list("product__name", flat=True)))
+
+    def test_delete_from_cart(self) -> None:
+        product = Product.objects.get(pk=ID)
+        CartItem.objects.create(
+            shopping_cart=self.user.shopping_cart,
+            product=product,
+            quantity=1
+        )
+        self.assertIn(product.name, list(self.user.shopping_cart.cart_items.values_list("product__name", flat=True)))
+
+        response = self.client.get(CART_DELETE_PRODUCT_URL)
+        self.assertEqual(response.status_code, 302)
+        self.assertNotIn(product.name, list(self.user.shopping_cart.cart_items.values_list("product__name", flat=True)))
