86666e22afa0f9dc12b52deb97a836eb13f4e090
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-23 13:49:58 +0300

    Add tests for Review form

--- store/tests/tests_forms.py
+++ store/tests/tests_forms.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from store.forms import ReviewForm
+from store.models import Product
+from store.tests.mixins import FixtureMixin
+
+
+class TestReviewForm(FixtureMixin, TestCase):
+    def setUp(self) -> None:
+        self.customer = get_user_model().objects.get(pk=1)
+        self.product = Product.objects.get(pk=1)
+
+    def test_form_valid(self) -> None:
+        form_data = {
+            "customer": self.customer,
+            "product": self.product,
+            "content": "Some content"
+        }
+        form = ReviewForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data["content"],
+            form_data["content"]
+        )
+
+    def test_form_invalid_no_content(self) -> None:
+        form_data = {
+            "customer": self.customer,
+            "product": self.product,
+        }
+        form = ReviewForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
