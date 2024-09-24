91a4d0cf05bdd5f1fd7eec366ae5b4bb100bcbd6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-24 23:47:55 +0300

    Change virt_env variables in settings

--- shop_wise/settings.py
+++ shop_wise/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
